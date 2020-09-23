7d0db96f70f72c3c039c334c25e11a6c11567de3
qzhang <zhangnetmail@163.com>
2020-09-23 15:39:41 +0800

    firtst commit

---
+++ cli.php
@@ -0,0 +1,8 @@
+<?php
+echo "this is cli client";
+$x=5;
+$y=6;
+$z=$x+$y;
+$z=$x+$z;
+//phpinfo();
+echo $z;
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=GBK">
+<title>Insert title here</title>
+</head>
+    <body>
+    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    <h1>this is my first php git project</h1>
+    <?php
+           //phpinfo();
+           echo "this is my first php git project<br/>";
+           $x=5;
+           $y=6;
+           $z=$x+$y;
+           $z=$x+$z;
+           echo $z;
+	?>
+    </body>
+</html>
